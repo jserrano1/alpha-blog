0f2375fcba6e7e56529a65f3d6b9b26d29c5abf6
jeremiah serrano <jserrano@demacmedia.com>
2017-06-15 20:21:00 +0000

    initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,48 @@
+class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @article = Article.new
+    end
+    def create
+        #render plain: params[:article].inspect
+        @article = Article.new(article_params)
+        @article.user = User.first
+        if @article.save
+            flash[:success] = "article was successfully created"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+    end
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+    def update
+        @article = Article.find(params[:id])
+        if @article.params
+            flash[:success] = "article was updated"
+            redirect_to article_path[@article]
+        else
+            render "edit"
+        end
+
+    end
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:danger] = "Article was successfully deleted"
+        redirect_to articles_path
+    end
+    private
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+
+end
